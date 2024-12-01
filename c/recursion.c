f708c0523d50871e551aa0f6f4332dd185cc2d77
Amey <ameygaikar28@gmail.com>
2024-12-01 20:25:55 +0530

    add recursion in c

---
+++ c/recursion.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void PrintN(char* str, int n) {
+  if (n == 1) {
+    return;
+  } else {
+    printf("%s\n", str);
+    PrintN(str, n - 1);
+  }
+}
+
+void PrintNumbersI(int n) {
+  if (n == 0) {
+    return;
+  } else {
+    printf("%d\n", n);
+    PrintNumbersI(n - 1);
+  }
+}
+
+void PrintNumbersII(int n, int c) {
+  if (c == n + 1) {
+    return;
+  } else {
+    printf("%d\n", c);
+    PrintNumbersII(n, c + 1);
+  }
+}
+
+int Addition(int n) {
+  if (n == 0) {
+    return 0;
+  } else {
+    return n + Addition(n - 1);
+  }
+}
+
+long long Factorial(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * Factorial(n - 1);
+  }
+}
+
+int BinarySearch(int nums[], size_t size, int target, int start, int end) {
+  if (start > end) {
+    return -1;
+  } else {
+    int mid = (start + end) / 2;
+    if (nums[mid] == target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      return BinarySearch(nums, size, target, mid + 1, end);
+    } else {
+      return BinarySearch(nums, size, target, start, mid - 1);
+    }
+  }
+}
